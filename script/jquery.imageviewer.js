fffc630fe0e22c2422a2eb27fe834abbeca343bf
georgepaterson <george.paterson@lbi.com>
2011-04-25 12:40:26 +0100

    Developing repository structure.

---
+++ script/jquery.imageviewer.js
@@ -0,0 +1,28 @@
+/*!
+ * jQuery Image Viewer plugin 0.0.1
+ *
+ * Copyright 2011, George Paterson
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ */
+(function($){
+	/*
+	 * 
+	 *
+	 */
+	$.fn.equaliser = function(options) {
+		if (options) {
+			settings = $.extend({}, $.fn.equaliser.defaults, options);
+		}
+	};
+	/*
+	 *	Default options, can be extend by options passed to the function.
+	 *
+	 */
+	$.fn.equaliser.defaults = {
+
+	};
+	$.fn.equaliser.methods = {
+
+	};
+})( jQuery );
